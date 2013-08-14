cfcfe2fee486fe6f2eb3a8784dc8a0434541ad1e
Ryan Kadwell <ryan@riaka.ca>
2013-08-14 07:32:59 -0400

    Moved functions out into Hanagram module

--- Hanagram.hs
+++ Hanagram.hs
@@ -3,4 +3,50 @@
 -- Copyright (c) Ryan Kadwell <ryan@riaka.ca>
 -- Author: Ryan Kadwell <ryan@riaka.ca>
 
-module Hanagram () where
+-- | Provides support for processing anagrams in haskell
+module Hanagram (
+      showMatches
+    , sayMatches
+    , getMatches
+    ) where
+
+import Data.List
+import Data.Tuple
+import System.Cmd
+import System.Exit
+
+-- | Display the matches to the user one per line
+showMatches :: [String] -> IO ()
+showMatches [] = do
+    putStrLn ""
+showMatches (x:xs) = do
+    putStrLn x
+    showMatches xs
+
+-- | Uses OSx text to speech to say the matches to the user
+sayMatches :: [[Char]] -> IO ExitCode
+sayMatches [] = system $ "echo ''"
+sayMatches (x:xs) = do
+    system $ "say " ++ x ++ " "
+    sayMatches xs
+
+-- | Takes a string of letters and a list of all possible words in the
+-- dictionary and returns the ones that can eb formed from the list
+getMatches :: String -> [String] -> [String]
+getMatches letters words =
+    [ word | word <- words, canMake word letters ]
+
+-- see if all letters in the word are in the array of letters
+canMake :: String -> String -> Bool
+canMake [] _ = True
+canMake (x:xs) letters = if x `elem` letters
+                         then canMake xs $ removeOne x letters
+                         else False
+
+-- remove one character from the string to give us matches that do not allow
+-- duplicate letters in the matching
+removeOne :: Char -> String -> String
+removeOne _ [] = []
+removeOne x letters = first ++ rest
+    where first = fst $ span (/= x) letters
+          rest = tail $ snd $ span (/= x) letters
--- main.hs
+++ main.hs
@@ -6,10 +6,7 @@
 module Main where
 
 import System.Environment (getArgs)
-import Data.List
-import Data.Tuple
-import System.Cmd
-import System.Exit
+import Hanagram (showMatches, sayMatches, getMatches)
 
 main :: IO ()
 main = do
@@ -21,35 +18,3 @@ main = do
     showMatches matches
     sayMatches matches
     putStrLn ""
-
-showMatches :: [String] -> IO ()
-showMatches [] = do
-    putStrLn ""
-showMatches (x:xs) = do
-    putStrLn x
-    showMatches xs
-
-sayMatches :: [[Char]] -> IO ExitCode
-sayMatches [] = system $ "echo ''"
-sayMatches (x:xs) = do
-    system $ "say " ++ x ++ " "
-    sayMatches xs
-
-getMatches :: String -> [String] -> [String]
-getMatches letters words =
-    [ word | word <- words, canMake word letters ]
-
--- see if all letters in the word are in the array of letters
-canMake :: String -> String -> Bool
-canMake [] _ = True
-canMake (x:xs) letters = if x `elem` letters
-                         then canMake xs $ removeOne x letters
-                         else False
-
--- remove one character from the string to give us matches that do not allow
--- duplicate letters in the matching
-removeOne :: Char -> String -> String
-removeOne _ [] = []
-removeOne x letters = first ++ rest
-    where first = fst $ span (/= x) letters
-          rest = tail $ snd $ span (/= x) letters
